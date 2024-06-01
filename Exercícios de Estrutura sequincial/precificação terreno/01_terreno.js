e892b1ecb2e3016012b0790502bc8be5ed1b7720
Renan Mota <renanmotadasilva@outlook.com>
2024-06-01 14:06:45 -0300

    alterações e inclusões no projeto

--- Exercícios de Estrutura sequincial/precificação terreno/01_terreno.js
+++ Exercícios de Estrutura sequincial/precificação terreno/01_terreno.js
@@ -1,24 +1,28 @@
+//pegando os elementos html pelos seus ids
 const larguraInput = document.getElementById('larguraInput');
 const comprimentoInput = document.getElementById('comprimentoInput');
-const valorMetroQuadradoInput = document.getElementById('valorMetroQuadradoInput');
-const resultadoArea = document.getElementById('resultadoArea');
-const resultadoPreco = document.getElementById('resultadoPreco');
+const valorM2Input = document.getElementById('valorM2Input');
+const areaDoTerreno = document.getElementById('areaDoTerreno');
+const precoDoTerreno = document.getElementById('precoDoTerreno');
 
-function calcularEExibir() {
-  const largura = larguraInput.value;
-  const comprimento = comprimentoInput.value;
-  const valorMetroQuadrado = valorMetroQuadradoInput.value;
 
-  // Verifica se algum dos valores é NaN
-  if (isNaN(largura) || isNaN(comprimento) || isNaN(valorMetroQuadrado)) {
-    resultadoArea.textContent = "Por favor, insira valores numéricos válidos.";
-    resultadoPreco.textContent = ""; // Limpa o resultado do preço
-    return; // Sai da função para evitar cálculos com valores inválidos
+function precificacaoDoTerreno(){
+  //.value é necessário para acessar o valor dentro do elemento html e não o elemento em sí.
+  const largura = parseFloat(larguraInput.value) 
+  const comprimento = parseFloat(comprimentoInput.value)
+  const valorM2 = parseFloat(valorM2Input.value)
+
+  //testando se algum valor é NaN
+  if(isNaN(largura)||isNaN(comprimento)||isNaN(valorM2)){
+    areaDoTerreno.textContent = "Atenção: Preencha os campos corretamente!"
+    precoDoTerreno.textContent = ""
+    console.log(areaDoTerreno)
+    return // aqui ele sai da função evitando cálculos desnecessários
   }
 
-  const area = largura * comprimento;
-  const preco = area * valorMetroQuadrado;
+  let area = (largura*comprimento).toFixed(2);
+  let valor = (area*valorM2).toFixed(2);
 
-  resultadoArea.textContent = `Área do terreno é de ${area.toFixed(2)} m²`;
-  resultadoPreco.textContent = `Valor total do terreno é de R$ ${preco.toFixed(2)}`;
+  areaDoTerreno.textContent = `Área do terreno: ${area} m²`
+  precoDoTerreno.textContent = `Preço do terreno: R$ ${valor}`
 }
